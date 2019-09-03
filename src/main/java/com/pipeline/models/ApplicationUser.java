716289e315c2d7b05b47c9bb981cc4ef073267fd
Steven Grant <blue.ninjaneer@gmail.com>
2019-09-03 16:33:23 -0700

    update for front end team to be able to do stuff

--- src/main/java/com/pipeline/models/ApplicationUser.java
+++ src/main/java/com/pipeline/models/ApplicationUser.java
@@ -31,7 +31,7 @@ public class ApplicationUser implements UserDetails {
     @ManyToOne
     CandidateGroup candidateGroup;
 
-    Set<ScheduledTask> scheduledTasks;
+//    Set<Progress> scheduledTasks;
 
 
     public long getId() {
